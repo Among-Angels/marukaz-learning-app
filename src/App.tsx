60bf9a244cfb26b537ce37886caf688fc31c9ba8
marukaz <kopamaru@gmail.com>
2020-12-22 23:37:18 +0900

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { tokenize } from "kuromojin";
+import React from "react";
+
 import "./App.css";
-
-const useInput = (initialValue: string) => {
-  const [value, set] = useState(initialValue);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    set(event.target.value);
-  };
-
-  return { value, onChange: handleChange };
-};
+import { useTextarea } from "./hooks/useTextarea";
+import { TransformedSentense } from "./transformedSentence";
 
 function App() {
-  const emailProps = useInput("");
-  const passwordProps = useInput("");
-  const [textareaValue, setTextareaValue] = useState("");
-  const [tokenized, setTokenized] = useState("");
-
-  useEffect(() => {
-    const timeOutId = setTimeout(
-      () =>
-        tokenize(textareaValue, {
-          dicPath: "/dict",
-        }).then((results) => {
-          setTokenized(results.map((x) => x["surface_form"]).join(" | "));
-        }),
-      700
-    );
-    return () => clearTimeout(timeOutId);
-  }, [textareaValue]);
+  const textareaProps = useTextarea("");
 
   return (
     <div className="App">
       <form>
-        <input type="email" {...emailProps} />
-        <input type="password" {...passwordProps} />
-        <textarea
-          value={textareaValue}
-          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
-            setTextareaValue(e.target.value)
-          }
-        />
+        <textarea {...textareaProps} />
       </form>
-      <p>{tokenized}</p>
+      <TransformedSentense value={textareaProps.value}></TransformedSentense>
     </div>
   );
 }
