f4b319571d6948aabac7fee1f087768345e18c90
marukaz <kopamaru@gmail.com>
2021-01-02 00:56:29 +0900

    Ant design

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,24 @@
 import React from "react";
+import { Input } from "antd";
 
 import "./App.css";
 import { useTextarea } from "./hooks/useTextarea";
 import { TransformedSentense } from "./transformedSentence";
 
 function App() {
-  const textareaProps = useTextarea("");
+  const { value, onChange } = useTextarea("");
+  const TextArea = Input;
 
   return (
     <div className="App">
-      <form>
-        <textarea {...textareaProps} />
-      </form>
-      <TransformedSentense value={textareaProps.value}></TransformedSentense>
+      <h1>notra</h1>
+      <TextArea
+        placeholder="Input text"
+        allowClear
+        value={value}
+        onChange={onChange}
+      />
+      <TransformedSentense value={value}></TransformedSentense>
     </div>
   );
 }
